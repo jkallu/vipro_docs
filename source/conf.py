401727d1a47d884f1b9ff4fc7ad8d5645167104b
jkallu <jinesh.bond@gmail.com>
2019-06-26 20:33:02 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -88,7 +88,7 @@ html_theme = "sphinx_rtd_theme"
 # html_theme_options = {}
 html_theme_options = {
     'canonical_url': '',
-    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    'analytics_id': 'UA-142452349-1',  #  Provided by Google in your dashboard
     'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
